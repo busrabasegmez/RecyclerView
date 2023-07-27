907ef9f7d3854e4048a1dca01496a9afefad39f9
busrabasegmez <busrabasegmez@gmail.com>
2023-07-27 16:32:20 +0300

    changes on photos

--- app/src/main/java/com/example/recyclerview/adapters/MainAdapter.kt
+++ app/src/main/java/com/example/recyclerview/adapters/MainAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.recyclerview.adapters
 
+import android.content.Intent
 import android.icu.number.NumberFormatter.with
 import android.icu.number.NumberRangeFormatter.with
 import android.view.LayoutInflater
@@ -7,11 +8,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
-import com.example.recyclerview.Channel
-import com.example.recyclerview.HomeFeed
-import com.example.recyclerview.R
+import com.example.recyclerview.*
 import com.squareup.picasso.Picasso
+import android.widget.Toast.makeText as makeText1
 
 
 class MainAdapter(val homeFeed: HomeFeed) : RecyclerView.Adapter<CustomViewHolder>() {
@@ -36,18 +37,27 @@ class MainAdapter(val homeFeed: HomeFeed) : RecyclerView.Adapter<CustomViewHolde
         val channel = video.channel
 
         holder.view.findViewById<TextView>(R.id.textView_video_title).text = video.name
-        holder.view.findViewById<TextView>(R.id.textView_channel_name).text = video.channel.name + "   •   " + "20k views \n 4 days ago"
+        holder.view.findViewById<TextView>(R.id.textView_channel_name).text =
+            video.channel.name + "   •   " + "20k views \n 4 days ago"
 
         val thumbnailImageView = holder.view.findViewById<ImageView>(R.id.imageView_video_thumbnail)
         Picasso.get().load(video.imageUrl).into(thumbnailImageView)
 
-        val channelProfileImageView = holder.view.findViewById<ImageView>(R.id.imageView_channel_profile)
+        val channelProfileImageView =
+            holder.view.findViewById<ImageView>(R.id.imageView_channel_profile)
         Picasso.get().load(channel.profileImageUrl).into(channelProfileImageView)
     }
 }
 
 class CustomViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
 
+    init {
+        view.setOnClickListener {
+            val intent = Intent(view.context, MainActivity::class.java)
+
+            view.context.startActivity(intent)
+        }
+    }
 }
 
 
